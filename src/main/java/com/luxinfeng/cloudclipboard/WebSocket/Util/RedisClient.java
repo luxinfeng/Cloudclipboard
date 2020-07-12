ae4e47045733eb34f7cebc5207b5e6edf2598e02
Lu <10568269+luxinfeng@users.noreply.github.com>
2020-07-12 18:53:25 +0800

    nothing change

--- src/main/java/com/luxinfeng/cloudclipboard/WebSocket/Util/RedisClient.java
+++ src/main/java/com/luxinfeng/cloudclipboard/WebSocket/Util/RedisClient.java
@@ -6,12 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.Jedis;
 
 
+
 public class RedisClient {
 
     public static Jedis getJedis(){
         return inner.instance;
     }
     private static class inner{
-        private static final Jedis instance = new Jedis("127.0.0.1", 6379,0);
+        private static final Jedis instance = new Jedis("127.0.0.1", 6379,0,0);
+
     }
 }
